18c1a8c75715090d822e9f3772eabb3be037df4b
Titaninette <letoffe.laetitia@gmail.com>
2020-10-04 11:32:48 +0200

    Update EndLevel Screen

--- src/ui/EndLevel.hx
+++ src/ui/EndLevel.hx
@@ -6,34 +6,59 @@ class EndLevel extends dn.Process {
 
 	var lvlData : LedData.LedData_Level;
 	var lvlInfo : Data.LevelInfo;
+
+	var flow : h2d.Flow;
 	
 	public function new() {
 		super(Game.ME);
 
+		lvlData = level.lvlData;
+		lvlInfo = level.lvlInfo;
+
 		createRootInLayers(parent.root, Const.DP_UI);
 
-		var flow = new h2d.Flow(root);
+		flow = new h2d.Flow(root);
 		flow.layout = Vertical;
 		flow.verticalSpacing = 20;
+		flow.horizontalAlign = Middle;
 
 		var endLevel = new h2d.Text(Assets.fontPixel, flow);
 		endLevel.text = "Your day is over...";
+		endLevel.setScale(Const.SCALE);
 
-		lvlData = level.lvlData;
-		lvlInfo = level.lvlInfo;
+		var flowScore = new h2d.Flow(flow);
+		flowScore.layout = Horizontal;
+		flowScore.horizontalSpacing = 20;
+
+		var score = new h2d.Text(Assets.fontPixel, flowScore);
+		score.text = 'Your score: ${level.currentTU}';
+
+		var maximumScore = new h2d.Text(Assets.fontPixel, flowScore);
+		maximumScore.text = 'Level maximum score: ${lvlInfo.maximumScore}';
 
 		var btn = new Button("Retry", function() {
 			game.retryLevel(lvlInfo);
 			destroy();
 		});
 		flow.addChild(btn);
-		if (!game.isLastLevel) {
-			var btn = new Button("Next", function() {
+	
+		if (!game.isLastLevel && level.currentTU <= lvlInfo.maximumScore) {
+			var btn = new Button("Next Level", function() {
 				game.nextLevel();
 				destroy();
 			});
 			flow.addChild(btn);
 		}
+
+		onResize();
 	}
 
+	override function onResize() {
+		super.onResize();
+
+		root.setScale(Const.SCALE);
+
+		flow.reflow();
+		flow.setPosition(Std.int((w() / Const.SCALE) - flow.outerWidth) >> 1, Std.int((h() / Const.SCALE) - flow.outerHeight) >> 1);
+	}
 }
