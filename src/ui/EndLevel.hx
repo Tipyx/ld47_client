fde84497fec4e63de4071992bcf061f051dda55a
Titaninette <letoffe.laetitia@gmail.com>
2020-10-05 10:52:43 +0200

    Update of EndLevel for a Good and Bad end

--- src/ui/EndLevel.hx
+++ src/ui/EndLevel.hx
@@ -8,6 +8,7 @@ class EndLevel extends dn.Process {
 	var lvlInfo : Data.LevelInfo;
 
 	var flow : h2d.Flow;
+	var endLevel : h2d.Text;
 	
 	public function new() {
 		super(Game.ME);
@@ -22,9 +23,43 @@ class EndLevel extends dn.Process {
 		flow.verticalSpacing = 20;
 		flow.horizontalAlign = Middle;
 
-		var endLevel = new h2d.Text(Assets.fontPixel, flow);
-		endLevel.text = "Your day is over...";
+		endLevel = new h2d.Text(Assets.fontPixel, flow);
+		// endLevel.text = "Your day is over...";
 		endLevel.setScale(Const.SCALE);
+
+		/* var flowScore = new h2d.Flow(flow);
+		flowScore.layout = Horizontal;
+		flowScore.horizontalSpacing = 20; */
+
+		/* var score = new h2d.Text(Assets.fontPixel, flowScore);
+		score.text = 'Your score: ${level.currentTU}';
+
+		var maximumScore = new h2d.Text(Assets.fontPixel, flowScore);
+		maximumScore.text = 'Level maximum score: ${lvlInfo.maximumScore}'; */
+
+		/* var btn = new Button("Retry", function() {
+			game.retryLevel(lvlInfo);
+			destroy();
+		});
+		flow.addChild(btn); */
+	
+		/* if (!game.isLastLevel && level.currentTU <= lvlInfo.maximumScore) {
+			var btn = new Button("Next Level", function() {
+				game.nextLevel();
+				destroy();
+			});
+			flow.addChild(btn);
+		} */
+
+		if (lvlInfo.maximumScore - level.currentTU <= 0) showRewindLevel();
+		else showEndLevel();
+		// TODO : remplacer les conditions de déclenchement des écrans de fin
+
+		onResize();
+	}
+
+	function showEndLevel() {
+		endLevel.text = "Your day is over!";
 
 		var flowScore = new h2d.Flow(flow);
 		flowScore.layout = Horizontal;
@@ -36,21 +71,21 @@ class EndLevel extends dn.Process {
 		var maximumScore = new h2d.Text(Assets.fontPixel, flowScore);
 		maximumScore.text = 'Level maximum score: ${lvlInfo.maximumScore}';
 
+		var btn = new Button("Next Level", function() {
+			game.nextLevel();
+			destroy();
+		});
+		flow.addChild(btn);
+	}
+
+	function showRewindLevel() {
+		endLevel.text = "You ran out of time...";
+
 		var btn = new Button("Retry", function() {
 			game.retryLevel(lvlInfo);
 			destroy();
 		});
 		flow.addChild(btn);
-	
-		if (!game.isLastLevel && level.currentTU <= lvlInfo.maximumScore) {
-			var btn = new Button("Next Level", function() {
-				game.nextLevel();
-				destroy();
-			});
-			flow.addChild(btn);
-		}
-
-		onResize();
 	}
 
 	override function onResize() {
